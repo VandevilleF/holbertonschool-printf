9b5f6065b7b7b8a386b52b424f370895d52492e3
VandevilleF <flo.vandeville@gmail.com>
2023-11-20 08:34:43 -0800

    add struct, function and conditions for %

--- 0-int_printf.c
+++ 0-int_printf.c
@@ -2,6 +2,28 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+
+typedef struct percent
+{
+	char *form;
+	void (*func_form)(va_list);
+} percent_func;
+
+void (printchar(va_list))
+{
+	printf("char");
+}
+
+void (printstr(va_list))
+{
+	printf("string");
+}
+
+void (putmodulo(va_list))
+{
+	printf("modulo");
+}
+
 /**
  * _printf - copy of fonction printf
  * @format: string you need to print
@@ -10,23 +32,29 @@
 
 int _printf(const char *format, ...)
 {
-	int i;
+	int i, j;
 	int n = strlen(format);
+	
+	percent_func find_percent[] = {
+		{"c", printchar},
+		{"s", printstr},
+		{"%", putmodulo},
+		{NULL, NULL}
+	};
 	
 	va_list arg;
 	va_start(arg, format);
 	
 	for (i = 0; i < n; i++)
 	{
-		if (format[i] == '%' && format[i + 1] == 's')
+		if (format[i] == '%')
 		{
-			printf("string");
-			i += 2;
-		}
-		if (format[i] == '%' && format[i + 1] == 'c')
-		{
-			printf("char");
-			i += 2;
+			if (format[i + 1] == *find_percent[0].form)
+				find_percent[0].func_form(arg);
+			if (format[i + 1] == *find_percent[1].form)
+				find_percent[1].func_form(arg);
+			if (format[i + 1] == *find_percent[2].form)
+				find_percent[2].func_form(arg);
 		}
 		putchar(format[i]);
 	}
