17bb0748495ffba25d3ec51e1a401cd65d11137c
JakShyrak <bourhlef.yacine53@gmail.com>
2023-11-22 12:04:05 +0100

    Update : Add "else" in our loop for increment and adding of the main function

--- 0-int_printf.c
+++ 0-int_printf.c
@@ -27,22 +27,43 @@ int _printf(const char *format, ...)
 	va_start(arg, format);
 	
 	for (i = 0; i < n; i++)
-	{
-		if (format[i] == '%')
-		{
-			int j = 0;
-			while (find_percent[j].form != 0)
-
-			{
-				if (format[i + 1] == *find_percent[j].form)
-					count += (find_percent[j].func_form(arg));
-				j++;
-			}
-			i += 2;
-		}
-		putchar(format[i]);
-		count++;
-	}
+{
+    if (format[i] == '%')
+    {
+        int j = 0;
+        while (find_percent[j].form != 0)
+        {
+            if (format[i + 1] == *find_percent[j].form)
+                count += (find_percent[j].func_form(arg));
+            j++;
+        }
+        i ++;  
+    }
+    else
+    {
+        putchar(format[i]);
+        count++;
+    }
+}
 	va_end(arg);
 	return (count);
+}
+
+
+/* NE PAS OUBLIER DE METTRE LE MAIN !! : */
+int main(void)
+{
+    int len;
+    int len2;
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    return (0);
 }
